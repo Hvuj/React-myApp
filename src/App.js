72fe1210e1237c92c573b50326735a68a066d871
Hvuj <56831016+Hvuj@users.noreply.github.com>
2020-03-17 07:55:52 +0200

    added component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Witcher from "./Witcher";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>my first react app</h1>
+				<p>welcome</p>
+			</header>
+			<Witcher />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Witcher.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Witcher extends Component {
+	render() {
+		return (
+			<div className="ninja">
+				<div>Name: Hvuj</div>
+				<div>age: 29</div>
+				<div>Belt: Black</div>
+			</div>
+		);
+	}
+}
+
+export default Witcher;
